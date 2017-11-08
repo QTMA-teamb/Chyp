a7b3ddb589227509892b10addce62ad8eb827ce1
Greg MacEachern <GregMac@Gregs-MacBook-Pro.local>
2017-11-08 12:11:09 -0500

    fixed background and package.json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './Logo.png';
 import './App.css';
 import Header from "./components/Header/Header.js"
 import Home from "./components/Home/Home.js"
+// import Events from "./components/Events/Events.js"
 import {
   BrowserRouter as Router,
   Route
@@ -17,15 +18,16 @@ class App extends Component {
         <Header />
         <main>
           <Route exact path="/" render={() => (
-            <div class = 'content'>
+            <div id = "mainn" class = 'content'>
               <h1>Welcome To Chyp</h1>
               <h2>Chyp lets you collect payments for a conference, party, or any other event in a click</h2>
               <h3 id= 'lblLogin'>Login With Facebook!</h3>
               <div id = "btnLogin" class="fb-login-button" data-width="200" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="true" onlogin="checkLoginState();"></div>
             </div>
           )}/>
-          <Route exact path="/events" component={Home}/>
           </main>
+          <Route exact path="/events" component={Home}/>
+
         </div>
       </Router>
     );
