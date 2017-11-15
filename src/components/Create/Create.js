a110af9251e0778f22c97ee2960943b67ec0a232
Greg  MacEachern <gregmaceachern@Gregs-MacBook-Pro-2.local>
2017-11-15 17:14:36 -0500

    Updated Card struct

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -7,15 +7,15 @@ class Card extends Component {
     return (
       <div class="blog-card spring-fever">
         <div class="title-content">
-          <h3><a href="#">BBQ at 234 Johnson!</a></h3>
-            <div class="intro"> <a>12/14/17</a> </div>
+          <h3><a href="#">Career Fair</a></h3>
+            <div class="intro">
+            <a>September 20, 2017</a><br></br>
+            <a>Grant Hall</a>
+            </div>
         </div>
       <div class="card-info">
-      Description of the event would go here Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim...
         <a href="#">Add To Chyp!<span class="licon icon-arr icon-black"></span></a>
       </div>
-    // <div class="gradient-overlay"></div>
-    // <div class="color-overlay"></div>
   </div>
     );
   }
