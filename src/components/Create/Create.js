0662340e44a4774881587d377c69d393734f0a37
Greg  MacEachern <gregmaceachern@Gregs-MacBook-Pro-2.local>
2017-11-16 19:12:48 -0500

    Event Support

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 import './Create.css';
 import '../../App.js'
 //
-// window.fbAsyncInit = function() {
-//   FB.init({
-//     appId            : '360886547672323',
-//     autoLogAppEvents : true,
-//     xfbml            : true,
-//     version          : 'v2.11'
-//   });
-// };
-//
-// (function(d, s, id){
-//    var js, fjs = d.getElementsByTagName(s)[0];
-//    if (d.getElementById(id)) {return;}
-//    js = d.createElement(s); js.id = id;
-//    js.src = "https://connect.facebook.net/en_US/sdk.js";
-//    fjs.parentNode.insertBefore(js, fjs);
-//  }(document, 'script', 'facebook-jssdk'));
+window.fbAsyncInit = function() {
+  FB.init({
+    appId            : '360886547672323',
+    autoLogAppEvents : true,
+    xfbml            : true,
+    version          : 'v2.11'
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
 
 
 class Card extends Component {
