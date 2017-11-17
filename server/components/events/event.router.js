077635ccd95cb6547b513305c06004e8a902a4d3
cam-perry <33005811+cam-perry@users.noreply.github.com>
2017-11-16 19:26:53 -0500

    Added events POST handler

--- server/components/events/event.router.js
+++ server/components/events/event.router.js
@@ -1,7 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
 
-router.get('/', getAllEvents)
+router.get('/', getAllEvents);
+router.post('/', createEvent);
+
+function createEvent(req, res) {
+
+  const db = require('../connectDB.js');
+  const con = db.connect();
+
+	con.connect(function(err) {
+	  if (err) throw err;
+	  console.log("Connected!");
+	  var sql = "INSERT INTO setup_events (admin_id, fb_event_id, description, e_name, loc_city, loc_country, loc_latitude, loc_longitude, loc_state, loc_street, loc_pc, start_time, cover) VALUES ?";
+	  var values = [
+            			[
+                    req.body.user_id,
+                    req.body.event.id,
+                    req.body.event.description,
+                    req.body.event.name,
+                    req.body.event.place.city,
+                    req.body.event.place.country,
+                    req.body.event.place.latitude,
+                    req.body.event.place.longitude,
+                    req.body.event.place.state,
+                    req.body.event.place.street,
+                    req.body.event.place.zip,
+                    req.body.event.start_time,
+                    req.body.event.cover.source
+                  ]
+            		];
+	  con.query(sql, [values], function (err, result) {
+		if (err) throw err;
+		console.log("Number of records inserted: " + result.affectedRows);
+	  });
+	});
+  res.status(200).send();
+}
 
 function getAllEvents(req,res){
   const data = [{
