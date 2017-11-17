295b244f7b54bed2ff99e0ce23b80f2381a5c91f
cam-perry <33005811+cam-perry@users.noreply.github.com>
2017-11-16 19:16:34 -0500

    Adding users to api router

--- server/components/api.router.js
+++ server/components/api.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const events = require('./events/event.router');
+const users = require('./users/user.router');
 
 router.use('/events', events);
-
+router.use('/users', users);
 
 
 
