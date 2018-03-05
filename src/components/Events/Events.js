6448f9a6ffc14a529dfcd1afb85fe1cc988650c9
Greg  MacEachern <gregmaceachern@Gregs-MacBook-Pro-3.local>
2018-03-04 22:00:32 -0500

    Filtered out events that aren't in the right city and were in the past

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -8,7 +8,7 @@ var newLocation;
 const events = [];
 
 function userLocation(){
-  var location = prompt("Please enter your location:", "Your Location");
+  var location = prompt("Please enter your city:", "Your Location");
   if (location == "") {
     alert('Please Enter A Valid Location');
     userLocation();
@@ -23,27 +23,36 @@ function userLocation(){
 }
 
 class Events extends Component {
-
-componentWillMount(){
+componentWillMount() {
   var recents = firebase.database().ref('events').orderByKey().limitToLast(15);
   recents.on('child_added', function(snapshot) {
     var ref = snapshot.val()
-      events.push(ref);
+    let eventTime = new Date(ref.end_time).getTime();
+    let currentTime = new Date().getTime();
+    if ((eventTime >= currentTime) && ref.place != null) {
+      if (ref.place.location != null) {
+        if (ref.place.location.city != null && ref.place.location.city == newLocation) {
+          events.push(ref);
+        }
+      }
+    }
+
   });
   console.log(events)
 }
-  componentDidMount() {
+componentDidMount() {
 
   newLocation = this.props.userlocation;
   if (!this.props.token) {
     window.location.href = "../";
     alert('Please Login With Facebook to Continue!')
     return
-  } else if (!this.props.userlocation) {
-    newLocation = userLocation();
   }
+  // } else if (!this.props.userlocation) {
+  //   newLocation = userLocation();
+  // }
+  newLocation = userLocation();
   console.log(newLocation);
-  document.getElementById("open").innerHTML = "Looking for events in " + newLocation;
 
 }
 
@@ -53,7 +62,7 @@ componentWillMount(){
         <div>
           <div class = "local">
             <h1 id = "localTitle">Local Events</h1>
-            <h2 id = "open">{newLocation}</h2>
+            <h2 id = "open">Looking for events in {newLocation}</h2>
             <div className = "Card">
             {events.map( (event) => (
             <CreateCard event={event} key={event.id} />
