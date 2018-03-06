1d1cb5813fa8757da2e0adc2601f163d7908cd6f
cam-perry <33005811+cam-perry@users.noreply.github.com>
2018-03-06 17:49:16 -0500

    Added button to landing page for juniors to style

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,13 +3,24 @@ import React, { Component } from 'react';
 import './LandingPage.css';
 
 export default class LandingPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.buttonClick = this.buttonClick.bind(this);
+  }
+
+  buttonClick(evt) {
+    window.alert('Button was clicked');
+  }
+
   render() {
     return (
       <div className='container' id='landing-page'>
         <div className='row'>
           <div className='col-12 col-md-6' id='text-col'>
-            <h1>Chyp</h1>
-            <h2>Painlessly collect payments for your events.</h2>
+            <h1>{'Chyp'}</h1>
+            <h2>{'Painlessly collect payments for your events.'}</h2>
+            <div role='button' onClick={this.buttonClick}>{'Start Here'}</div>
           </div>
           <div className='col-12 col-md-6' id='phone-col'>
             iPhone
