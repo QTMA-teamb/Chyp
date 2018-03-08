5cbd077b1fd55e5954c5e36918626aaace311b51
cam-perry <33005811+cam-perry@users.noreply.github.com>
2018-03-07 20:09:18 -0500

    Triggering Facebook login click for new landing page buton

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -10,7 +10,7 @@ export default class LandingPage extends React.Component {
   }
 
   buttonClick(evt) {
-    window.alert('Button was clicked');
+    document.getElementById('btnLogin').click()
   }
 
   render() {
