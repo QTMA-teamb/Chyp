57a34a540926fd20ee829ea135413b29c16dcfc2
Huiyi Yang <13hy22@queensu.ca>
2018-03-20 20:12:32 -0400

    modified popup.js

--- src/components/PopUp/popUp.js
+++ src/components/PopUp/popUp.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import "./popUp.css"
 import fire from '../../fire.js';
 import { withRouter } from 'react-router';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, InputGroup, InputGroupAddon, Input } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 
 class ModalExample extends React.Component {
   constructor(props) {
@@ -40,10 +40,10 @@ class ModalExample extends React.Component {
         stage: 'stripe'
       })
     });
+    document.getElementsByClassName('modal-footer')[0].remove();
   }
 
   render() {
-
     return (
       <div>
         <Button id = "Create" className="initButton" onClick={this.toggle}>Add To Chyp!</Button>
@@ -51,15 +51,15 @@ class ModalExample extends React.Component {
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>{'Set a Ticket Price for this Event!'}</ModalHeader>
           <ModalBody id = "information">
-
+        
              { this.state.stage === 'price'
               ? <InputGroup>
-                  <InputGroupAddon addonType="prepend">$</InputGroupAddon>
-                  <Input type="number" step="1" onChange={this.handleChange} placeholder="Ticket Price" />
-                  <InputGroupAddon addonType="append">.00</InputGroupAddon>
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="basic-addon1">$</span>
+                    </div>
+                    <Input type="number" step="1" onChange={this.handleChange} placeholder="Ticket Price" />
                 </InputGroup>
-
-              : <a href={"https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_CRiyeuFWnWEukuvXDGVIDP7ggGdMbm5r&scope=read_write&state=" + fire.auth().currentUser.uid} className="stripe-connect"><span>{'Connect with Stripe'}</span></a>
+              : <div><p><a href={"https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_CRiyeuFWnWEukuvXDGVIDP7ggGdMbm5r&scope=read_write&state=" + fire.auth().currentUser.uid} className="stripe-connect"><span>{'Connect with Stripe'}</span></a></p><p>Please add your bank account to Stripe so that we can send you your ticket sales.</p></div>
              }
 
           </ModalBody>
