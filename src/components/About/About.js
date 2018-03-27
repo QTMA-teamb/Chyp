1de58e1766d668d2506174e0745cbba3ec9d7a5c
Huiyi Yang <13hy22@queensu.ca>
2018-03-27 19:19:49 -0400

    Made about page mobile-friendly

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,33 +6,37 @@ class About extends Component {
 
   render() {
     return (
-      <div class= "main">
+    <div class= "main">
         <h1>About Our Team</h1><br/>
-        <div class = 'pplCont'>
-          <div class = "cam">
+        <div class="container">
+        <div class="row">
+          <div class = "col-sm-3 cam">
             <h2><b>Product Manager:</b><br/> Cameron Perry</h2>
             <h3>Commerce</h3>
             <img src={require('./cam.png')}/>
           </div>
-          <div class = "greg">
+          <div class = "col-sm-3 greg">
             <h2><b>Software Developer:</b><br/> Greg MacEachern</h2>
             <h3>Computer Engineering</h3>
             <img src={require('./greg.png')}/>
           </div>
-          <div class = "tim">
+          <div class = "col-sm-3 tim">
             <h2><b>Junior Analyst:</b><br/> Timothy Than</h2>
             <h3>Commerce</h3>
             <img src={require('./tim.png')}/>
           </div>
-          <div class = "brian">
+          <div class = "col-sm-3 brian">
             <h2><b>Junior Analyst:</b><br/> Brian Colbert</h2>
             <h3>Commerce</h3>
             <img src={require('./brian.png')}/>
           </div>
         </div>
+        <div>
         <address>
           Contact us: <a href="mailto:chypit.ca@gmail.com">chypit.ca@gmail.com</a>
         </address>
+        </div>
+        </div>
 
       </div>
 
